9c74710d61e0a658efa4e8f51e408078704dd226
remythoer <remy.thoer@u-psud.fr>
2017-04-28 15:44:33 +0200

    time normalisation and temperature display

--- main.cpp
+++ main.cpp
@@ -11,12 +11,14 @@ int main(int argc, char *argv[])
 {
   TApplication app("appplication",NULL,NULL); 
   Int_t j1(0), j2(0); /// \brief count the number of rootfiles
-   
   ifstream HistoInputs;
   vector<string> peakref;
   vector<vector<Int_t> > valeur_integrale;
   vector<vector<Int_t> > valeur_integrale_cold;
-
+  Double_t time1(0), time2(0);
+  Double_t temp(0), Errtemp(0);
+  Histogram warm("./ForRemy/RunForAnalysis/Warm_night_2015-04-21--62067");
+  
   struct dirent* openfile = NULL;
   DIR* dir = NULL;
   dir = opendir("./ForRemy/RunForAnalysis/Warm"); /// \brief open directory
@@ -39,7 +41,7 @@ int main(int argc, char *argv[])
 	      string ligne;
 	      HistoInputs.open("./ForRemy/HistoForAnalysis.txt");
 	      if(HistoInputs)
-		{
+		{ 
 		  while(getline(HistoInputs, ligne))
 		    {
 		      istringstream iss (ligne);
@@ -51,20 +53,19 @@ int main(int argc, char *argv[])
 			  peakref.push_back(ref);
 			  valeur_integrale.push_back(vector<Int_t>(4,0));
 			}
-		      
 		      Int_t Integrale = histo.Integrate(detector,bin_g,bin_d-bin_g,bin_g-max_bg,bin_d+max_bg);
 		      Int_t k=i-detector*23;
 		      valeur_integrale[k][detector]=valeur_integrale[k][detector]+Integrale;
-		      
+		      time1=time1+histo.Get_time();
 		      
 		      i++;
 		    }
 		}
 	      else {cout<<"Error file "<<name<<" not open"<<endl;}
 	      HistoInputs.close();
-	      
+	      time1=time1+histo.Get_time();
+	      	      
 	      cout<<name<<" "<<j1<<endl;
-
 	      j1++;
 	    }
 	}
@@ -100,77 +101,36 @@ dir = opendir("./ForRemy/RunForAnalysis/Cold"); /// \brief open directory
 		      iss >> detector >> bin_g >> bin_d >> min_bg >> max_bg >> ref; 
 		      if(j2==0 && detector==0)
 			{
-			  
 			  valeur_integrale_cold.push_back(vector<Int_t>(4,0));
 			}
-		      
 		      Int_t Integrale = histo.Integrate(detector,bin_g,bin_d-bin_g,bin_g-max_bg,bin_d+max_bg);
 		      Int_t k=i-detector*23;
 		      valeur_integrale_cold[k][detector]=valeur_integrale_cold[k][detector]+Integrale;
-		      
 		      
 		      i++;
 		    }
 		}
 	      else {cout<<"Error file "<<name<<" not open"<<endl;}
 	      HistoInputs.close();
+	      time2=time2+histo.Get_time();
+	      histo.temp1(warm, 1, temp, Errtemp);
 	      
-	      cout<<name<<" "<<j2<<endl;
-
+	      cout<<name<<" "<<j2<<" T="<<temp<<"+/-"<<Errtemp<<endl;
 	      j2++;
 	    }
 	}
     }
-     
+  closedir(dir);
   cout<<"out"<<endl;
-
-
-
-
-  // Histogram histo_cold("./ForRemy/RunForAnalysis/Cold_2015-04-22-62133");
-  // Histogram warm("./ForRemy/RunForAnalysis/Warm_night_2015-04-21--62067");
-  // Double_t temp, errTemp;
-  // histo_cold.temp1(warm,1,temp,errTemp);
-  // cout<<"T="<<temp<<"+/-"<<errTemp<<endl;
-  
-  
-  // HistoInputs.open("./ForRemy/HistoForAnalysis.txt");
-  // if(HistoInputs)
-  //   {
-  //     Int_t i(0);
-  //     string ligne;
-  //     while(getline(HistoInputs, ligne))
-  // 	{
-  // 	  istringstream iss (ligne);
-  // 	  Int_t detector, bin_g, bin_d, min_bg, max_bg;
-  // 	  string ref;
-  // 	  iss >> detector >> bin_g >> bin_d >> min_bg >> max_bg >> ref; 
-  // 	  if(detector==0)
-  // 	    {
-	     
-  // 	      valeur_integrale_cold.push_back(vector<Int_t>(4,0));
-  // 	    }
-		      
-  // 	  Int_t Integrale = histo_cold.Integrate(detector,bin_g,bin_d-bin_g,bin_g-max_bg,bin_d+max_bg);
-  // 	  Int_t k=i-detector*23;
-  // 	  valeur_integrale_cold[k][detector]=valeur_integrale_cold[k][detector]+Integrale;
-		      
-		      
-  // 	  i++;
-  // 	}
-  //   }
-  // else{cout<<"error file cold not open"<<endl;}
-  // HistoInputs.close();
   
   ofstream filewarm;
   filewarm.open("file_warm.dat");
-  
   for(Int_t i=0; i<peakref.size(); i++)
     {
-      valeur_integrale[i][0]=valeur_integrale[i][0]/j1;
-      valeur_integrale[i][1]=valeur_integrale[i][1]/j1;
-      valeur_integrale[i][2]=valeur_integrale[i][2]/j1;
-      valeur_integrale[i][3]=valeur_integrale[i][3]/j1;
+      valeur_integrale[i][0]=valeur_integrale[i][0]/time1;
+      valeur_integrale[i][1]=valeur_integrale[i][1]/time1;
+      valeur_integrale[i][2]=valeur_integrale[i][2]/time1;
+      valeur_integrale[i][3]=valeur_integrale[i][3]/time1;
       
       filewarm<<peakref[i]<<" "<<valeur_integrale[i][0]<<" "<<valeur_integrale[i][1]<<" "<<valeur_integrale[i][2]<<" "<<valeur_integrale[i][3]<<endl;
     }
@@ -178,19 +138,17 @@ dir = opendir("./ForRemy/RunForAnalysis/Cold"); /// \brief open directory
 
   ofstream filecold;
   filecold.open("file_cold.dat");
-  
   for(Int_t i=0; i<peakref.size(); i++)
     {
-      valeur_integrale_cold[i][0]=valeur_integrale_cold[i][0]/j2;
-      valeur_integrale_cold[i][1]=valeur_integrale_cold[i][1]/j2;
-      valeur_integrale_cold[i][2]=valeur_integrale_cold[i][2]/j2;
-      valeur_integrale_cold[i][3]=valeur_integrale_cold[i][3]/j2;
+      valeur_integrale_cold[i][0]=valeur_integrale_cold[i][0]/time2;
+      valeur_integrale_cold[i][1]=valeur_integrale_cold[i][1]/time2;
+      valeur_integrale_cold[i][2]=valeur_integrale_cold[i][2]/time2;
+      valeur_integrale_cold[i][3]=valeur_integrale_cold[i][3]/time2;
 
       filecold<<peakref[i]<<" "<<valeur_integrale_cold[i][0]<<" "<<valeur_integrale_cold[i][1]<<" "<<valeur_integrale_cold[i][2]<<" "<<valeur_integrale_cold[i][3]<<endl;
     }
   filecold.close();
 
   //app.Run();
-
   return 0;
 }

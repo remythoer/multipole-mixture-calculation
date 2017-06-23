25832caac36e232654d8a36b35e13a237c7ff460
remythoer <remy.thoer@u-psud.fr>
2017-06-23 15:30:24 +0200

    ajout d'une raie

--- main.cpp
+++ main.cpp
@@ -22,13 +22,14 @@ int main(int argc, char *argv[])
   Double_t time1[4]={0};
   Double_t time2[4]={0};
   Double_t temp(0), Errtemp(0);
-  //Histogram warm("./ForRemy/RunForAnalysis/Warm_night_2015-04-21--62067");
+  Histogram warm("./ForRemy/RunForAnalysis/Warm_night_2015-04-21--62067");
   Histogram_constructed sum_warm ("./sum_warm");
   Histogram_constructed sum_cold ("./sum_cold");
   Histogram first_cold ("./ForRemy/RunForAnalysis/RMN_MN_2015-04-22-185000-62101");
   Histogram first_warm ("./ForRemy/RunForAnalysis/Warm_night_2015-04-21--61984");
   Double_t lambda=1.03824729*pow(10,-7);
   
+  Double_t Temptot(0), ErrTemptot(0);
   
 
     struct dirent* openfile = NULL;
@@ -56,7 +57,7 @@ int main(int argc, char *argv[])
 	      	     
 	      for(Int_t j=0; j<4; j++){time1[j]=time1[j]+(histo.Get_time()-histo.Get_deadtime()[j])*histo.Get_activity(first_warm,lambda);} /// \brief deadtime of warm runs	      	      
 	      cout<<name<<" "<<j1<<endl;
-	      cout<<"calibration detector 3 a="<<histo.Calibrate()[3][0]<<" b="<<histo.Calibrate()[3][1]<<" c="<<histo.Calibrate()[3][2]<<endl;
+	      // cout<<"calibration detector 3 a="<<histo.Calibrate()[3][0]<<" b="<<histo.Calibrate()[3][1]<<" c="<<histo.Calibrate()[3][2]<<endl;
 	      j1++;
 	    }
 	}
@@ -84,15 +85,22 @@ dir = opendir("./ForRemy/RunForAnalysis/Cold"); /// \brief open directory
 	      sum_cold+=histo;
 	      
 	      for(Int_t j=0; j<4; j++){time2[j]=time2[j]+(histo.Get_time()-histo.Get_deadtime()[j])*histo.Get_activity(first_warm,lambda);}/// \brief deadtime of cold runs	      	      
-	      //histo.temp1(warm, 1, temp, Errtemp);
+	      histo.temp1(warm, 1, temp, Errtemp);
 	      cout<<name<<" "<<j2<<endl;
-	      cout<<"calibInv detector 3 a="<<histo.CalibInv()[3][0]<<" b="<<histo.CalibInv()[3][1]<<" c="<<histo.CalibInv()[3][2]<<endl;
+	      // cout<<"calibInv detector 3 a="<<histo.CalibInv()[3][0]<<" b="<<histo.CalibInv()[3][1]<<" c="<<histo.CalibInv()[3][2]<<endl;
+	      cout<<"temperature = "<<temp<<" +/- "<<Errtemp<<endl;
+	      Temptot=Temptot+temp;
+	      ErrTemptot=ErrTemptot+Errtemp*Errtemp;
 	      j2++;
 	    }
 	}
     }
+  
+  
   closedir(dir);
   cout<<"out"<<endl;
+
+  cout<<"Temptot="<<Temptot/j2<<" +/- "<<sqrt(ErrTemptot)/j2<<endl;
   ofstream filewarm;
   ofstream filecold;
   ofstream filew;
@@ -122,7 +130,7 @@ dir = opendir("./ForRemy/RunForAnalysis/Cold"); /// \brief open directory
 	  Double_t ErrCold(0), ErrWarm(0);
 	  Int_t Integrale_cold = sum_cold.Integrate(detector,bin_g,bin_d-bin_g+1,bin_g-max_bg,bin_d+max_bg,ErrCold);
 	  Int_t Integrale_warm = sum_warm.Integrate(detector,bin_g,bin_d-bin_g+1,bin_g-max_bg,bin_d+max_bg,ErrWarm);
-	  Int_t k=i-detector*25; /// \brief 25 pour fe56 et 5 pour fe57
+	  Int_t k=i-detector*27; /// \brief 27 pour fe56 et 5 pour fe57
 	  valeur_integrale_cold_sum[k][detector]=Integrale_cold;
 	  valeur_integrale_warm_sum[k][detector]=Integrale_warm;
 	  Err_integrale_cold[k][detector]=ErrCold;
